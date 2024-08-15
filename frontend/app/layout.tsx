d3bfcbe9f1dad6a42b0ca561359200d0aa6fa40b
Welf9i <welfkimedamine@gmail.com>
2024-08-15 16:40:20 +0100

    add head icon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,13 +3,15 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from "@/components/theme-provider"
 import { ClerkProvider } from '@clerk/nextjs'
-import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'AI Chatbot',
   description: 'Your personal AI assistant',
+  icons:{
+    icon: '/google-gemini-icon.svg'
+  }
 }
 
 export default function RootLayout({
@@ -20,9 +22,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <Head>
-          <link rel="icon" href="/ai-avatar.png" type="image/png" />
-        </Head>
+        <head>
+          <link rel="icon" href="/google-gemini-icon.svg"/>
+        </head>
         <body className={inter.className}>
           <ThemeProvider
             attribute="class"
